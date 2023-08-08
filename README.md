d4b51c878f0f51637d8601be042024925c20118c
Larissa Nathanna Gatto <larissangatto@gmail.com>
2023-08-07 18:59:32 -0600

    Primeira versão do sistema bancário

---
+++ README.md
@@ -0,0 +1,29 @@
+# 🏦 Sistema bancário - versão 1  - Regras de funcionamento.
+
+O sistema conta apenas com um usuário não é preciso identificá-lo (agencia e conta bancária).
+
+## 💸Saque
+
+    -  O sistema deve permitir somente 3 saques diários.
+
+    -  Limite máximo de 500 reias por saque.
+
+    -  Caso o cliente não tenha saldo em conta deve imprimir uma mesagem informando.
+
+    -  Saques armazenados e mostrados no extrato.
+
+    -  O caixa não possui moedas por isso o saque deve ser realizado com valores inteiros.(Não negativos pois não tem sentido.)
+
+## 💰Depósito
+
+    - Deve ser possível somente depositar valores positivos e inteiros.
+
+    - Depositos armazendos e mostrados no extrato
+
+## 💲Visualização de extrato
+
+    -  Deve mostrar todos os depositos e saques.
+
+    -  Deve mostrar o saldo atual da conta.
+
+    -  Formato R$ xxxx.xx
---
+++ desafio_dio_sistema_bancario-V1.py
@@ -0,0 +1,91 @@
+# importando bibliotecas
+import datetime
+import time
+
+#criando o menu inicial
+print("""\nOlá Usuário do Banco XX:
+Por favor digitar a operação que você deseja realizar. No menu inicial \n\n""")
+
+menu_inicial = """
+................. Menu Inicial...............
+[d] Depositar
+[s] Sacar
+[e] Exibir extrato
+[q] Sair
+...............................................
+"""
+# iniciando variaveis da conta do cliente
+saldo = 0
+limite_saque = 500
+extrato  = ""
+numero_saques = 0
+limite_saques = 3
+# código sem considerar operações mecanicas.
+while True:
+    opcao =  input(menu_inicial)
+
+    if opcao == "d":
+        print("\n-Você digitou acessou a função:\n- Depósito.\n")
+        deposito = input("\nDigite o valor do depósito: R$ ")
+        try:
+            deposito = int(deposito)
+            if deposito >=0:
+                data = datetime.datetime.now()
+                print(f"\n - O valor a ser depositado é : R$ {int(deposito)}.00.\n\n-Insira o dinheiro no local indicado pelo caixa com o símbolo $.\n")
+                saldo = saldo + deposito
+                extrato = extrato + (f"-Deposito: R${deposito},00 as {data.strftime('%H:%M - %d.%m.%Y')}\n")
+                time.sleep(3)
+                print(f"\n***-Depósito: R$ {deposito}.00 efetuado com sucesso as {data.strftime('%H:%M - %d.%m.%Y')}!")
+                print("\n\n-Você será direcionado ao menu inicial!\n")
+                time.sleep(2)
+        except:
+            print("\n###-Falha na operação: O caixa só aceita valores inteiros.")
+            print("\n-Você será direcionado ao menu inicial.\n")
+            time.sleep(2)   
+        
+          
+    elif opcao == "s":
+        print("\n-Você digitou acessou a função:\n- Saque.\n")
+        saque = input("\nDigite o valor que deseja sacar: R$ ")
+        try:
+            saque = int(saque)
+            if saque > saldo:
+                print("\n###-Falha da operação: Saldo insuficiente!")
+                print("\n\n-Você será direcionado ao menu inicial!\n")
+                time.sleep(2)
+            elif numero_saques >= limite_saques: 
+                print("\n###-Falha da operação: Numero de saques diário já foi atingido.")
+                print("\n\n-Você será direcionado ao menu inicial!\n")
+                time.sleep(2)
+            elif saque > limite_saque:
+                print("\n###-Falha da operação: O Valor limite para cada saque é de R$ 500,00.")
+                print("\n\n-Você será direcionado ao menu inicial!\n")
+                time.sleep(2)
+            elif saque >= 0:
+                data = datetime.datetime.now()
+                print(f"\n- O valor a ser sacado é : R$ {int(saque)},00.\n\n-Retire o dinheiro no local indicado pelo caixa com o símbolo $.\n")
+                numero_saques += 1
+                time.sleep(3)
+                print(f"\n***-Saque de R$ {saque}.00 efetuado com sucesso as {data.strftime('%H:%M - %d.%m.%Y')}")
+                print("\n\n-Você será direcionado ao menu inicial!\n")
+                time.sleep(2)
+                saldo = saldo - saque
+                extrato = extrato + (f"Saque: R${saque}.00 as {data.strftime('%H:%M - %d.%m.%Y')} \n")
+        except:
+            print("\n###-Falha da operação: O caixa não possui moedas e só aceita valores inteiros.")
+            print("\n\n-Você será direcionado ao menu inicial.\n")
+            time.sleep(2)
+    
+    elif opcao == "e":
+        data = datetime.datetime.now()     
+        if extrato == "":
+            print(f"=========================Extrato========================:\n\n-Você  não realizou nenhuma trasação hoje {data.strftime('%d.%m.%Y')}\n  Seu saldo atual é de R$ {saldo}.00!\n================================================================== \n\n-Você será direcionado ao menu inicial! \n")
+        else:
+            print(f"=========================Extrato========================:\n\n-Você realizou as seguintes operações:\n{extrato}\n  Seu saldo atual é R$ {saldo}.00 as {data.strftime('%H:%M - %d.%m.%Y')}!\n=========================================================== \n\n-Você será direcionado ao menu inicial!\n")
+    
+    elif opcao =="q":
+        print("***Obrigado por utilizar nosso serviço eletrônico.\n")
+        break
+
+    else:
+        print("### - Operação inválida, por favor selecione a operação desejada no menu inicial!\n")
